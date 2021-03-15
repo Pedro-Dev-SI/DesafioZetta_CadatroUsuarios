e9824c69507c599d853aebb7386534fa44ba6693
Pedro Selvate <pedroselvate72@gmail.com>
2021-03-15 14:24:30 -0300

    Metodos DELETE e POST implementados

--- apirest/src/main/java/com/usuarios/apirest/resource/UsuarioResource.java
+++ apirest/src/main/java/com/usuarios/apirest/resource/UsuarioResource.java
@@ -3,9 +3,11 @@ package com.usuarios.apirest.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,7 @@ public class UsuarioResource {
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
-	//Lista todos os usuarios da tabela
+	//Lista todos os usuarios da tabela(banco de dados)
 	@GetMapping("/usuarios")
 	public List<Usuario> listaUsuarios(){
 		return usuarioRepository.findAll();
@@ -32,8 +34,23 @@ public class UsuarioResource {
 		return usuarioRepository.findById(id);
 	}
 	
+	//Adiciona um novo usuario na tabela(banco de dados)
 	@PostMapping("/usuario")
 	public Usuario salvaUsuario(@RequestBody Usuario usuario) {
 		return usuarioRepository.save(usuario);
 	}
+	
+	//Remove uma linha(usuario) da tabela(banco de dados)
+	@DeleteMapping("/usuario")
+	public void deletaUsuario(@RequestBody Usuario usuario) {
+		usuarioRepository.delete(usuario);
+	}
+	
+	//Atualizar um usuario no banco de dados
+	@PutMapping("/usuario")
+	public Usuario editaUsuario(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	
 }
